5b1aa3e132eb72f018be7a4f9d89b7f14c72d665
AlTawhid205 <altawhid205@gmail.com>
2026-02-01 01:25:35 +0600

    into

---
+++ arguments.js
@@ -0,0 +1,13 @@
+function doubleIt(number){
+  const double=number*2;
+  console.log(number, double);
+}
+console.log("funtion call here");
+doubleIt(10);
+doubleIt(20);
+doubleIt(30);
+const money=500;
+doubleIt(money);
+
+
+
