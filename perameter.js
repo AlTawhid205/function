57d1f08fb871943850dd1afc43dfdcd25aebcb47
AlTawhid205 <altawhid205@gmail.com>
2026-02-01 01:10:22 +0600

    js funtion start

---
+++ perameter.js
@@ -0,0 +1,14 @@
+function vatKhaw(){
+  console.log("aso");
+  console.log("Boso");
+  console.log("vat new");
+  console.log("Gopat gopta kore khaw");
+}
+// vatKhaw();
+
+
+
+function add(number,){
+
+}
+add();
